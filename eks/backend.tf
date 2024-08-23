9007ac04385c6fe21db9d5c5be5aea4a6997562f
Iddrisu08 <160420835+Iddrisu08@users.noreply.github.com>
2024-08-23 01:49:00 -0400

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,8 +10,8 @@ terraform {
     bucket         = "my-ews-baket6"
     region         = "us-east-2"
     key            = "eks/terraform.tfstate"
-    #dynamodb_table = "Lock-Files"
-    #encrypt        = true
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
   }
 }
 
